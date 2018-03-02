e5b495c8f478b29a655df9e1aa63593f7dd18d36
Rafael Profeta <rebaellion@gmail.com>
2018-03-02 07:18:42 -0300

    Modificação no programa

--- redefatec.sh
+++ redefatec.sh
@@ -2,18 +2,13 @@
 #trocando de rede, troca de proxy
 user="***"
 password="*********"
-proxyFull="https_proxy=http://2840481723"$user":"$password"@172.16.0.1:3128/"
-proxyNull="https_proxy"
-proxy=""
+proxy="https_proxy=http://2840481723"$user":"$password"@172.16.0.1:3128/"
 rede="search wifi.fatecrp.local"
 ip=`cat /etc/resolv.conf | grep wifi`
 if [ "$ip" == "$rede" ]
 then
-	proxy="$proxyFull"
 	sudo echo $proxy >> /etc/environment
 else
-
-	proxy="$proxyNull"	
 	sudo sed -i '/proxy/d' /etc/environment  
 fi
 source /etc/environment
